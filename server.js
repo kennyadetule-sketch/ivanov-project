442bf820f57d290852f5d5d317ed0793b51e963f
Kenny Adetule <kennyadetule@gmail.com>
2026-05-12 21:49:47 +0100

    Final production updates

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ app.post('/request', async (req, res) => {
 
     try {
       await resend.emails.send({
-       from: 'Ivanov <noreply@resend.dev>',
+       from: 'Ivanov <onboarding@resend.dev>',
         to: [process.env.EMAIL_USER],
         replyTo: email,
         subject: `New ${service} Request from ${name}`,
